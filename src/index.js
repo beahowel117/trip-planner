a25a3f9d8e158d3d099b63477232ddca222f87f6
Lilly Oh <ohlilly@gmail.com>
2020-01-27 12:25:29 -0500

    trip planner

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const mapboxgl = require("mapbox-gl");
+
+mapboxgl.accessToken = "pk.eyJ1IjoibGlsbHlvaCIsImEiOiJjazV3cHB1a3gwMHZlM29yN3J6cjlxOWtxIn0.rKJNVJhadZ6cqy6Z-S5Tog";
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
